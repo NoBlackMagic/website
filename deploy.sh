f218668cb7f2d696aebe5cac829042742bbce01e
marcopeg <marco.pegoraro@gmail.com>
2015-02-09 09:26:51 +0100

    deploy the build to the main website repository

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
+#
+# build the website
+# update the master branch in "NoBlackMagic/noblackmagic.github.io"
+# clean up
+#
+
+gulp build
+
 mkdir deploy
 cd deploy
 
 # clone a new version of the repository
 git clone git@github.com:NoBlackMagic/noblackmagic.github.io.git
 cd noblackmagic.github.io
-git checkout wks
-
-# build the product
-gulp build
-mv build ../build
+git checkout master
 
 # clean existing branch
-git checkout master
 mv .git ../
 find ./ -mindepth 1 -delete
 mv ../.git ./
 
 # add & commit
-mv ../build/dev/* .[^.]* ./
+mv ../../build/dev/* .[^.]* ./
 git add -A .
 git commit -m "publish"
 
